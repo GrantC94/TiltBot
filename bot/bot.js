1ca09a33ddb5d23daa629c7d6f924f5be82585b8
GrantC94 <grantwc94@gmail.com>
2019-12-09 21:30:14 -0600

    Try this I guess

--- bot/bot.js
+++ bot/bot.js
@@ -196,7 +196,7 @@ function sendEachLine(filename) {
 }
 
 function checkGames() {
-  sendEachLine("../Resources/summoners.txt")
+  sendEachLine("./resources/summoners.txt")
 }
 
 /*function sendEachLineInitialize(filename) {
